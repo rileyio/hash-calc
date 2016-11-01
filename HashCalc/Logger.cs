bc8a7954cdc06bdf176cc80c00a48a8782d71b67
Jared Booker <jay@tdm.io>
2016-11-01 01:11:04 -0300

    Adding previously missed files from previous commit

---
+++ HashCalc/Logger.cs
@@ -0,0 +1,131 @@
+﻿// cslog (C# Log)
+// Simple application logger.
+//
+// author: Jared Booker <TheJaydox>
+// license: MIT
+// https://github.com/TheJaydox/cslog
+//
+
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslog
+{
+    public enum ErrorLevel
+    {
+        Info,
+        Error,
+        Exception
+    }
+
+    public class Logger
+    {
+        // Version of the Logger Class
+        private static string Version = "1.0";
+
+        // Task & Collection
+        private static BlockingCollection<LogEntry> Queue = new BlockingCollection<LogEntry>();
+        private static bool TaskCreated = false;
+        private static Task WriterTask;
+
+        // Called on startup to populate initial line
+        public static string Init = "cslog: " + Version;
+
+        // File to log to
+        public static string LogFile = "cslog.log";
+
+        public static void Log(string message, ErrorLevel level = ErrorLevel.Info, [CallerMemberName]string caller = "")
+        {
+            //Console.WriteLine("Adding to queue: " + entry.Message);
+            Queue.Add(new LogEntry(level, caller, message));
+            StartWriterTask();
+        }
+
+        public static void Object<T>(T obj, ErrorLevel level = ErrorLevel.Info, [CallerMemberName]string caller = "")
+        {
+            Queue.Add(new LogEntry(level, caller, toJSON<T>(obj)));
+            StartWriterTask();
+        }
+
+        public static void Exception(Exception ex, [CallerMemberName]string caller = "")
+        {
+            Queue.Add(new LogEntry(ErrorLevel.Exception, caller, ex.Message + ":" + ex.StackTrace));
+            StartWriterTask();
+        }
+
+        private static string toJSON<T>(T obj)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream();
+            serializer.WriteObject(ms, obj);
+
+            // Create JSON string for logging
+            return Encoding.Default.GetString(ms.ToArray());
+        }
+
+        private static void StartWriterTask()
+        {
+            Console.WriteLine("Start Writer Called!");
+            if (TaskCreated == false)
+            {
+                Console.WriteLine("Logger Task Started!");
+
+                // Update Task Status
+                TaskCreated = true;
+
+                WriterTask = Task.Factory.StartNew(() =>
+                    Writer(),
+                    TaskCreationOptions.LongRunning
+                );
+            }
+        }
+
+        private static void Writer()
+        {
+            using (var writer = File.AppendText(LogFile))
+            {
+                foreach (var item in Queue.GetConsumingEnumerable())
+                {
+                    writer.WriteLine(StringOut(item));
+                    writer.Flush();
+                }
+            }
+        }
+
+        private static string StringOut(LogEntry entry)
+        {
+
+            Console.WriteLine(entry.Message);
+
+            string output = String.Format("{0} :: {1} [{2}] {3}",
+                entry.DT,
+                entry.Level,
+                entry.Caller,
+                entry.Message);
+
+            return output;
+        }
+    }
+
+    public class LogEntry
+    {
+        public DateTime DT { get; set; }
+        public string Level { get; set; }
+        public string Caller { get; set; }
+        public string Message { get; set; }
+
+        public LogEntry(ErrorLevel level, string caller, string message)
+        {
+            this.DT = DateTime.UtcNow;
+            this.Level = level.ToString();
+            this.Caller = caller;
+            this.Message = message;
+        }
+    }
+
+}
---
+++ HashCalc/Writer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace HashCalc
+{
+    class Writer
+    {
+        private FileInfo InitialFile;
+        private FileHashes Hashes;
+        private string Destination;
+
+        private void WriteSingleFile()
+        {
+            using (TextWriter writer = File.CreateText(String.Format("{0}\\{1}.sums", Destination, InitialFile.Name)))
+            {
+                // Write file name & details
+                writer.WriteLine(String.Format("Name:\t{0}", InitialFile.Name));
+                writer.WriteLine(String.Format("Bytes:\t{0}", InitialFile.Length));
+
+                // Insert Blank line
+                writer.WriteLine("");
+
+                // Write each Hash Algorithm from Collection (FIFO order list)
+                foreach (FileHash sum in Hashes.Hashes)
+                {
+                    writer.WriteLine(String.Format("{0}\t{1}", sum.Algorithm, sum.Value));
+                }
+
+                writer.Close();
+            }
+        }
+
+        internal Writer(string destination, FileInfo file, FileHashes fileHashes, bool single)
+        {
+            // Set Class properties
+            this.InitialFile = file;
+            this.Destination = destination;
+            this.Hashes = fileHashes;
+
+            if (single == true) { WriteSingleFile(); }
+        }
+    }
+}
